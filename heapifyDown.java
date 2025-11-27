037575fe7837826524f53e259c19901c59e9ee8b
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-11-27 23:44:11 +0530

    Add solution for Exercise 1: heapifyDown

---
+++ heapifyDown.java
@@ -0,0 +1,21 @@
+void heapifyDown(int[] arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapifyDown(arr, n, largest);
+    }
+}
+
